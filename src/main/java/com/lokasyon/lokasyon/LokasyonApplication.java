88ae2f9c35a9c191cd703774e4aa36ed2184ad8b
seymakcakaya <seymasuha00@gmail.com>
2024-03-15 21:49:35 +0300

    Initial commit

---
+++ src/main/java/com/lokasyon/lokasyon/LokasyonApplication.java
@@ -0,0 +1,13 @@
+package com.lokasyon.lokasyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokasyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokasyonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokasyon/lokasyon/LokasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokasyon.lokasyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokasyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
