9c88da1aaafc67acf36d1547a5df131dfbda2a82
seymakcakaya <seymasuha00@gmail.com>
2024-03-17 18:28:36 +0300

    NeighborhoodNotFoundException yapısına varargs eklendi
    (cherry picked from commit 8cce4ad1ddfe90bdb7060da39914c03f43db7ed6)

--- src/main/java/com/lokasyon/lokasyon/exception/NeighborhoodNotFoundException.java
+++ src/main/java/com/lokasyon/lokasyon/exception/NeighborhoodNotFoundException.java
@@ -1,7 +1,7 @@
 package com.lokasyon.lokasyon.exception;
 
 public class NeighborhoodNotFoundException extends RuntimeException {
-    public NeighborhoodNotFoundException(String message){
+    public NeighborhoodNotFoundException(String message,Object... args){
         super(message);
     }
 }
