6f655695579a717ffed18e32dd851d498ffefccb
seymakcakaya <seymasuha00@gmail.com>
2024-03-17 18:27:16 +0300

    Method isimleri ve api açıklamaları güncellendi
    (cherry picked from commit 2102629baed494e56c914928297dd217b21e8a68)

--- src/main/java/com/lokasyon/lokasyon/controller/LokasyonController.java
+++ src/main/java/com/lokasyon/lokasyon/controller/LokasyonController.java
@@ -29,40 +29,40 @@ public class LokasyonController {
 
     @GetMapping("/cities")
     @Cacheable("cities")
-    @Operation(summary = "List Of City", description = "Bu method  veritabanındaki tüm şehirleri listeler")
+    @Operation(summary = "Şehirler", description = "Bu method  veritabanındaki tüm şehirleri listeler")
     public ResponseEntity<List<CityDto>> getCities() {
         return ResponseEntity.ok(lokasyonService.getAllCitys());
     }
 
     @GetMapping("/{city}/districts")
-    @Operation(summary = "List Of district", description = "Bu method belirli şehire ait tüm ilçeleri listeler")
+    @Operation(summary = "İlçe Bilgileri", description = "Bu method belirli şehire ait tüm ilçeleri listeler")
     public ResponseEntity<List<DistrictDto>> getAllDistricts(@PathVariable("city") String city) {
         List<DistrictDto> districList=lokasyonService.getDistrictsByCity(city);
         return  new ResponseEntity<>(districList, HttpStatus.OK);
     }
 
     @GetMapping("/neighborhoods")
-    @Operation(summary = "List Of neighborhood", description = "Bu method ilçelere ait mahalleri listeler")
-    public ResponseEntity<List<NeighborhoodDto> >getAlleighborhood(@RequestParam(value = "city") String city, @RequestParam(value = "town") String town) {
-        return ResponseEntity.ok(lokasyonService.getNeighborhoodsByTown(city, town));
+    @Operation(summary = "Mahalle Bilgileri", description = "Bu method şehir ve semt bilgilerine göre mahalleri listeler")
+    public ResponseEntity<List<NeighborhoodDto> >getAllNeighborhood(@RequestParam(value = "city") String city, @RequestParam(value = "town") String town) {
+        return ResponseEntity.ok(lokasyonService.getNeighborhoodsByTownAndCity(city, town));
     }
     @GetMapping("/locations")
-    @Operation(summary = "List Of Location", description = "Bu method posta konuna göre tüm lokasyonları  listeler")
-    public ResponseEntity<List<LocationDto>> getLocationByZipCode(@RequestParam(value = "code") String code ) {
+    @Operation(summary = "Detaylı Lokasyon Bilgisi", description = "Bu method posta kodunun bağlı oladuğu tüm lokasyonları gösterir.")
+    public ResponseEntity<List<LocationDto>> findLocationsByZipCode(@RequestParam(value = "code") String code ) {
 
-            List<LocationDto>locationDtoList =  lokasyonService.getLocationByZipCode(code);
+            List<LocationDto>locationDtoList =  lokasyonService.findLocationsByZipCode(code);
             return new ResponseEntity<>(locationDtoList,HttpStatus.OK);
 
     }
     @GetMapping("/{city}/locations")
-    @Operation(summary = "Lokasyon Bilgileri", description = "Bu method  belirli şehirdeki tüm lokasyonları listeler")
-    public ResponseEntity<List<LocationDto>> getLocations(@PathVariable("city") String city) {
-        return ResponseEntity.ok(lokasyonService.getLocations(city));
+    @Operation(summary = "Şehir Lokasyon Bilgileri", description = "Bu method  belirli şehirdeki tüm lokasyonları listeler")
+    public ResponseEntity<List<LocationDto>> getAllLocationsByCity(@PathVariable("city") String city) {
+        return ResponseEntity.ok(lokasyonService.getAllLocationsByCity(city));
     }
     @GetMapping("/district/{zipcode}")
-    @Operation(summary = "ilçe Bilgileri", description = "Bu method girilen zip koda ait ilçe ve şehir ve semt  bilgilerini gösterir.")
-    public ResponseEntity<LocationDto> getDistictByZipCode(@PathVariable("zipcode") String zipCode) {
-        return ResponseEntity.ok(lokasyonService.getCityAndDistrictAndTownByZipCode(zipCode));
+    @Operation(summary = "Lokasyon Bilgisi", description = "Bu method girilen posta kodunun bağlı olguğu  il,ilçe ve semt bilgilerini gösterir.")
+    public ResponseEntity<LocationDto> getLocationInfoByZipCode(@PathVariable("zipcode") String zipCode) {
+        return ResponseEntity.ok(lokasyonService.getLocationInfoByZipCode(zipCode));
     }
 
 }
