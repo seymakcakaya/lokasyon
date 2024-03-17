2d371c95c5c23579cf7d0d06856b34292c8819a6
seymakcakaya <seymasuha00@gmail.com>
2024-03-17 18:32:43 +0300

    @Entity notasyonu eklendi

--- src/main/java/com/lokasyon/lokasyon/entity/Lokasyon.java
+++ src/main/java/com/lokasyon/lokasyon/entity/Lokasyon.java
@@ -1,10 +1,7 @@
 package com.lokasyon.lokasyon.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 
@@ -15,6 +12,7 @@ import lombok.*;
 public class Lokasyon {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String city;
 
